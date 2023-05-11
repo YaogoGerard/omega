a6d8429b1ada5c6dea5b24b65dde1a337cf81ca4
Gerard <gerardwyaogo@gmail.com>
2023-05-11 20:41:55 +0100

    Beta

--- omega/vues/info.py
+++ omega/vues/info.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 def create_frame(parent):
-    info =tk.Frame(parent, bg="black")
-    label = tk.Label(info, text="je suis la page info")
-    label.pack(padx=10, pady=10)
+    info =tk.Frame(parent, bg="#800040")
+    label = tk.Message(info,width=500, text="Ce logiciel est destion destiné au Centre de Develeppement pour Enfants(CDE), il a été realiser par YAOGO Gérard Windpagnangdé du CDE BF-780.\n ceci est la première version du logiciel: la version 1, ce logiciel ne doit etre vendu en aucun cas , il a été developper pour faciliter le travail pour les responsables et agents de COMPASSION INTERNNAL.")
+    label.pack(padx=10,pady=200)
     return info
